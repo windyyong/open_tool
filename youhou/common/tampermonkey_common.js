38123f5480377d74b2ea43b4898a4d5fa1c4e29b
zhangjunyong <530650681@qq.com>
2026-01-30 11:39:12 +0800

    add 1.添加通用地址

--- youhou/common/tampermonkey_common.js
+++ youhou/common/tampermonkey_common.js
@@ -105,8 +105,10 @@ function warehouse(_address) {
         return '自家仓库';
     } if (_address.includes("深圳")) {
         return '洁红家';
-    } else if (_address.includes("山门大道700号") || (_address.includes("广州") && _address.includes("化龙镇"))) {
+    } else if (_address.includes("山门大道700号") || ( _address.includes("化龙镇"))) {
         return '广州700'
+    } else if (_address.includes("永大路99号")) {
+        return '广州99'
     } else if (_address.includes("广兴路9999号") || (_address.includes("广州") && _address.includes("南沙"))) {
         return '广州9999'
     } else if (_address.includes("临空北路100号") || (_address.includes("武汉") && _address.includes("横店"))) {
